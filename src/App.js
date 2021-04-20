88bd9462b823893e22cf55066e6c9882aff03a11
kurtykurtykurty <30372576+kurtykurtykurty@users.noreply.github.com>
2021-04-20 19:11:33 +0300

    lesson 18

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
+import Dialogs from "./components/Dialogs";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
+import style from "./index.module.css";
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <Profile />
+      <div className={style.content}>
+        {/* <Profile /> */}
+        <Dialogs />
+      </div>
     </div>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import style from "./index.module.css";
 
 const Navbar = () => {
-  return <nav className={style.navbar}>Navbar</nav>;
+  return (
+    <nav className={style.navbar}>
+      <div>Profile</div>
+      <div>Dialogs</div>
+    </nav>
+  );
 };
 
 export default Navbar;
