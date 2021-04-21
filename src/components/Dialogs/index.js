c621eaeb6f0e1373be0154826faf499d0ccf0869
kurtykurtykurty <30372576+kurtykurtykurty@users.noreply.github.com>
2021-04-21 18:10:55 +0300

    lesson 22 route exact

--- src/components/Dialogs/index.js
+++ src/components/Dialogs/index.js
@@ -1,11 +1,20 @@
+import { NavLink } from "react-router-dom";
 import style from "./index.module.css";
 
 const Dialogs = (props) => {
   return (
     <div className={style.dialogs}>
       <div className={style.dialogsItems}>
-        <div className={style.dialog}>Joka</div>
-        <div className={style.dialog}>Boka</div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/1" className={style.dialog}>
+            Joka
+          </NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/2" className={style.dialog}>
+            Boka
+          </NavLink>
+        </div>
       </div>
       <div className={style.messages}>
         <div className={style.message}>hi</div>
