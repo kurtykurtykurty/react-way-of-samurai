ae504efabe5369a461b585d9d5f03fbca3e861eb
kurtykurtykurty <30372576+kurtykurtykurty@users.noreply.github.com>
2021-04-21 17:51:26 +0300

    lesson 21 layout dialogs

--- src/components/Dialogs/index.js
+++ src/components/Dialogs/index.js
@@ -1,5 +1,18 @@
-const Dialogs = () => {
-  return <div>Dialogs</div>;
+import style from "./index.module.css";
+
+const Dialogs = (props) => {
+  return (
+    <div className={style.dialogs}>
+      <div className={style.dialogsItems}>
+        <div className={style.dialog}>Joka</div>
+        <div className={style.dialog}>Boka</div>
+      </div>
+      <div className={style.messages}>
+        <div className={style.message}>hi</div>
+        <div className={style.message}>yo</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
