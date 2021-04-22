5870261dc9045a6483e4ca2b614042c7b28415e4
kurtykurtykurty <30372576+kurtykurtykurty@users.noreply.github.com>
2021-04-22 21:06:57 +0300

    lesson 35, observer, subscribe, callback

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,7 @@
-import rerenderEntireTree from "../render";
+let rerenderEntireTree = () => {
+  console.log("state changed");
+};
+
 let state = {
   profilePage: {
     posts: [
@@ -25,7 +28,7 @@ let state = {
   },
 };
 
-export let addPost = () => {
+export const addPost = () => {
   let newPost = {
     id: 5,
     message: state.profilePage.newPostText,
@@ -37,9 +40,13 @@ export let addPost = () => {
   rerenderEntireTree(state);
 };
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
   state.profilePage.newPostText = newText;
   rerenderEntireTree(state);
+};
+
+export const subscribe = (observer) => {
+  rerenderEntireTree = observer;
 };
 
 export default state;
