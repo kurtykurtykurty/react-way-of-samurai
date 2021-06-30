07c9b8e983f2a83fd0018be39f1bab02ac3ae21b
kurtykurtykurty <30372576+kurtykurtykurty@users.noreply.github.com>
2021-06-30 18:28:13 +0300

    Users API (installing axios, get /users)

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,11 +1,20 @@
 /* eslint-disable react/button-has-type */
+import * as axios from 'axios';
 import style from './Users.module.css';
 
 const User = (props) => {
     return (
         <div className={style.container}>
             <div>
-                <img src={props.photo} className={style.userpic} alt="" />
+                <img
+                    src={
+                        props.photo != null
+                            ? props.photo
+                            : 'https://блоха.com/sites/default/files/styles/avatar/public/userpic.png'
+                    }
+                    className={style.userpic}
+                    alt=""
+                />
                 <div>
                     {props.isFollowed ? (
                         <button
@@ -40,35 +49,40 @@ const Users = (props) => {
     const state = props.usersPage;
 
     if (state.users.length === 0) {
-        props.setUsers([
-            {
-                id: '1',
-                followed: true,
-                fullName: 'Хуёмоё',
-                status: 'Хуятус',
-                location: { city: 'Блядск', country: 'Ебаландия' },
-                photoUrl:
-                    'https://блоха.com/sites/default/files/styles/avatar/public/userpic.png',
-            },
-            {
-                id: '2',
-                followed: false,
-                fullName: 'Еблан',
-                status: 'Хуятус',
-                location: { city: 'Блядск', country: 'Ебаландия' },
-                photoUrl:
-                    'https://блоха.com/sites/default/files/styles/avatar/public/userpic.png',
-            },
-            {
-                id: '3',
-                followed: false,
-                fullName: 'Чмоня',
-                status: 'Блятус',
-                location: { city: 'Чмоньск', country: 'Чмостан' },
-                photoUrl:
-                    'https://блоха.com/sites/default/files/styles/avatar/public/userpic.png',
-            },
-        ]);
+        axios
+            .get('https://social-network.samuraijs.com/api/1.0/users')
+            .then((response) => {
+                props.setUsers(response.data.items);
+            });
+        // props.setUsers([
+        //     {
+        //         id: '1',
+        //         followed: true,
+        //         fullName: 'Хуёмоё',
+        //         status: 'Хуятус',
+        //         location: { city: 'Блядск', country: 'Ебаландия' },
+        //         photoUrl:
+        //             'https://блоха.com/sites/default/files/styles/avatar/public/userpic.png',
+        //     },
+        //     {
+        //         id: '2',
+        //         followed: false,
+        //         fullName: 'Еблан',
+        //         status: 'Хуятус',
+        //         location: { city: 'Блядск', country: 'Ебаландия' },
+        //         photoUrl:
+        //             'https://блоха.com/sites/default/files/styles/avatar/public/userpic.png',
+        //     },
+        //     {
+        //         id: '3',
+        //         followed: false,
+        //         fullName: 'Чмоня',
+        //         status: 'Блятус',
+        //         location: { city: 'Чмоньск', country: 'Чмостан' },
+        //         photoUrl:
+        //             'https://блоха.com/sites/default/files/styles/avatar/public/userpic.png',
+        //     },
+        // ]);
     }
 
     const UserList = state.users.map((u) => {
@@ -76,9 +90,9 @@ const Users = (props) => {
             <User
                 id={u.id}
                 key={u.id}
-                name={u.fullName}
+                name={u.name}
                 status={u.status}
-                photo={u.photoUrl}
+                photo={u.photos.small}
                 isFollowed={u.followed}
                 follow={(id) => {
                     props.follow(id);
